8690341bde35e0f2d16231db7fc08c54f66b3459
Sano206 <gajdos.alexander206@gmail.com>
2021-01-04 21:45:57 +0100

    fixed breadcrumbs and whatever probably done

---
+++ gasparovic_script.js
@@ -0,0 +1,22 @@
+$(function (){
+    $(".piece_1, .piece_2, .piece_3, .piece_4, .piece_5, .piece_6, .piece_7, .piece_8, .piece_9, .piece_10").draggable({
+        containment: 'parent',
+        revert: true
+    });
+
+
+    var seconds = 0, minutes = 0, hours = 0, timer = setInterval(function () {
+        seconds++;
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+        if (minutes === 60) {
+            hours++;
+            minutes = 0;
+        }
+        document.getElementById('output').innerHTML = hours + ' : ' + minutes + ' : ' + seconds;
+    }, 1000);
+
+
+});
